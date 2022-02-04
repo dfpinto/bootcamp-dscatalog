ce1da97b92ade8e2b8468338befe815538c68af8
dfpinto <dirley.figueredo@gmail.com>
2022-02-03 23:54:26 -0300

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,22 @@
+import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
   return (
-    <h1>Hello DSCatalog</h1>
+    <>
+      <Navbar />
+      <h1>Hello DSCatalog</h1>
+    </>
   );
 }
+
+/*
+   a função acima também poderia ser escrita com lambda:
+
+   const App () => {
+     return <h1>Hello DSCatalog</h1>;
+   }
+*/
 
 export default App;
