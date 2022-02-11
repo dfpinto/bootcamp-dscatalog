1baa70114d3ed9763698f2a0cfb560dd06cf124e
dfpinto <dirley.figueredo@gmail.com>
2022-02-11 20:08:48 -0300

    Home and Catalog styling. Routing.

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,12 +1,11 @@
-import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
+import Routes from 'Routes';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <h1>Hello DSCatalog</h1>
+      <Routes />
     </>
   );
 }
