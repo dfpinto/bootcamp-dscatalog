1241ed28ace8ce80fd2d99bb0783ec1d709acd0d
dfpinto <dirley.figueredo@gmail.com>
2022-02-23 22:30:39 -0300

    ProductDetails layout mobile

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,7 +1,35 @@
+import {ReactComponent as ArrowIcon} from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
+import './style.css';
+
 const ProductDetails = () => {
-    return (
-        <h1>Página ProductDetails</h1>
-    )
-}
+  return (
+    <div className="product-details-container">
+      <div className="base-card product-details-card">
+        <div className="goback-container">
+            <ArrowIcon/>
+            <h1>VOLTAR</h1>
+        </div>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+                <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Nome do Produto" />
+            </div>
+            <div className="name-price-container">
+                <h2>Nome do Produto</h2>
+                <ProductPrice price={2998.99}/>
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+                <h3>Descrição do Produto</h3>
+                <p>descrição do pruduto detalhamento</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProductDetails;
