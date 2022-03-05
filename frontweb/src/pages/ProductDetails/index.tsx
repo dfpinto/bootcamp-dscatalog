6d664e367c2e94fb07de93aa72160d3828f1ae62
dfpinto <dirley.figueredo@gmail.com>
2022-03-04 23:24:17 -0300

    FORMA INCORRETA DE USAR O AXIOS / CHAMADA URI BACK END

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,9 +1,23 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import axios from 'axios';
 import ProductPrice from 'components/ProductPrice';
 import { Link } from 'react-router-dom';
+import { Product } from 'types/products';
+import { isThisTypeNode } from 'typescript';
+import { BASE_URL } from 'util/requests';
 import './style.css';
 
 const ProductDetails = () => {
+
+  // FORMA INCORRETA
+  let product: Product;
+
+  // FORMA INCORRETA
+  axios.get(BASE_URL + "/products/2")
+  .then(response => {
+       console.log(response.data)
+    });
+
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
