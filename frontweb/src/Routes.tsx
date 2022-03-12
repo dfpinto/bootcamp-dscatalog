e0e2904dc9dcff6e86934f213fd86a4c80f10eef
dfpinto <dirley.figueredo@gmail.com>
2022-03-12 11:57:47 -0300

    Auth routes

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -1,9 +1,10 @@
 import Navbar from 'components/Navbar';
 import Home from 'pages/Home';
 import Catalog from 'pages/Catalog';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Router, Switch } from 'react-router-dom';
 import ProductDetails from 'pages/ProductDetails';
 import Admin from 'pages/Admin';
+import Auth from 'pages/Admin/Auth';
 
 const Routes = () => {
   return (
@@ -18,6 +19,10 @@ const Routes = () => {
         </Route>
         <Route path="/products/:productId">
           <ProductDetails />
+        </Route>
+        <Redirect from="/admin/auth" to="/admin/auth/login" exact/>
+        <Route path="/admin/auth">
+          <Auth/>
         </Route>
         <Redirect from="/admin" to="/admin/products" exact/>
         <Route path="/admin">
