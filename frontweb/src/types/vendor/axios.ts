cd5efb1a28216cb1375b1ce81e23208c41202338
dfpinto <dirley.figueredo@gmail.com>
2022-03-15 22:28:41 -0300

    Fix AxiosRequestConfig

--- frontweb/src/types/vendor/axios.ts
+++
@@ -1,8 +0,0 @@
-import { Method } from "axios";
-
-export type AxiosParams = {
-    method?: Method; // quando não informato assume GET
-    url: string;
-    data?: object;
-    params?: object;
-}
