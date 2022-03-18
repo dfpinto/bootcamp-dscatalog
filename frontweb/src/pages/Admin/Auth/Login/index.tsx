c3e3223d42b9705f1a4b43217c46a0d7093fd06b
dfpinto <dirley.figueredo@gmail.com>
2022-03-17 22:04:12 -0300

    Form validation

---
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -0,0 +1,80 @@
+import { Link } from 'react-router-dom';
+import ButtonIcon from 'components/ButtonIcon';
+import { useForm } from 'react-hook-form';
+import './styles.css';
+import { RequestBackendLogin } from 'util/requests';
+import { useState } from 'react';
+
+type FormData = {
+  username: string;
+  password: string;
+}
+ 
+const Login = () => {
+
+  const [hasError, setHasError] = useState(false);
+  const {register, handleSubmit, formState:{errors}} = useForm<FormData>();
+  const onSubmit = (formData: FormData) => {
+    RequestBackendLogin(formData).then(response => {
+      console.log('SUCESSO', response);
+      setHasError(false);
+    }).catch(error => {
+      console.log('ERRO', error);
+      setHasError(true);
+    })
+  }
+
+  return (
+    <div className="base-card login-card">
+      <h1>LOGIN</h1>
+      {hasError && (
+      <div className="alert alert-danger">
+        Erro ao tentar efetuar o login
+      </div>)}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-4">
+          <input
+            {...register("username", {
+              required: 'Campo obrigatório',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'Email inválido'
+              }
+            })}
+            type="text"
+            className="form-control base-input"
+            placeholder="Email"
+            name="username"
+          />
+          <div className="invalid-feedback d-block">{errors.username?.message}</div>
+        </div>
+        <div className="mb-2">
+          <input
+            {...register("password", {
+              required: 'Campo obrigatório'
+            })}
+            type="password"
+            className="form-control base-input "
+            placeholder="Password"
+            name="password"
+          />
+          <div className="invalid-feedback d-block">{errors.password?.message}</div>
+        </div>
+        <Link to="/admin/auth/recover" className="login-link-recover">
+          Esqueci a senha
+        </Link>
+        <div className="login-submit">
+          <ButtonIcon text="Fazer login" />
+        </div>
+        <div className="signup-container">
+          <span className="not-registered">Não tem Cadastro?</span>
+          <Link to="/admin/auth/register" className="login-link-register">
+            CADASTRAR
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+ 
+export default Login;
