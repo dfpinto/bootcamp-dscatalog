468f2e8786de985b44cbf7f48adbf5f77f88d5e6
Dirley Pinto <dirley.figueredo@gmail.com>
2022-05-16 23:16:31 -0300

    Mocking function with spyOn

---
+++ frontweb/src/util/auth.spec.ts
@@ -0,0 +1,20 @@
+import { hasAnyRole } from "./auth";
+import * as TokenModule from "../util/token";
+
+describe("hasAnyRole tests", () => {
+    test("should return true when empty list", () => {
+        const result = hasAnyRole([]);
+        expect(result).toEqual(true);
+    })
+
+    test.only("should return true when user has given role", () => {
+
+        jest.spyOn(TokenModule,'getTokenData').mockReturnValue({
+            exp: 0,
+            user_name: '',
+            authorities: ["ROLE_ADMIN","ROLE_OPERATOR"]
+        })
+        const result = hasAnyRole(['ROLE_ADMIN']);
+        expect(result).toEqual(true);
+    })
+})
