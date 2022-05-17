86cfbd4b99d9cd3ade8a1dfd303b8fbeb1c0c0f5
Dirley Pinto <dirley.figueredo@gmail.com>
2022-05-16 23:22:46 -0300

    Testing another hasAnyRole scenario

--- frontweb/src/util/auth.spec.ts
+++ frontweb/src/util/auth.spec.ts
@@ -7,7 +7,7 @@ describe("hasAnyRole tests", () => {
         expect(result).toEqual(true);
     })
 
-    test.only("should return true when user has given role", () => {
+    test("should return true when user has given role", () => {
 
         jest.spyOn(TokenModule,'getTokenData').mockReturnValue({
             exp: 0,
@@ -17,4 +17,15 @@ describe("hasAnyRole tests", () => {
         const result = hasAnyRole(['ROLE_ADMIN']);
         expect(result).toEqual(true);
     })
+
+    test("should return fase when user does not have given role", () => {
+
+        jest.spyOn(TokenModule,'getTokenData').mockReturnValue({
+            exp: 0,
+            user_name: '',
+            authorities: ["ROLE_OPERATOR"]
+        })
+        const result = hasAnyRole(['ROLE_ADMIN']);
+        expect(result).toEqual(false);
+    })    
 })
