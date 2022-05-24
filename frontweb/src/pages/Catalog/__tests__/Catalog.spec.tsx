30ad6f1d87975bb3ce9dd37e9c8bb2ae58352451
Dirley Pinto <dirley.figueredo@gmail.com>
2022-05-23 21:05:17 -0300

    MSW mock server, fixtures

--- frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -2,6 +2,11 @@ import { render, screen, waitFor } from "@testing-library/react";
 import { Router } from "react-router-dom";
 import Catalog from "..";
 import history from "util/history";
+import { server } from "../fixtures";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers);
+afterAll(() => server.close);
 
 test('should render Catalog', async () => {
     render(
@@ -13,7 +18,7 @@ test('should render Catalog', async () => {
     expect(screen.getByText("Catálogo de Produtos")).toBeInTheDocument();
 
     await waitFor(() => {
-        expect(screen.getByText("Smart TV")).toBeInTheDocument();
+        expect(screen.getByText("PC Gamer Tr")).toBeInTheDocument();
     })
 
 });
