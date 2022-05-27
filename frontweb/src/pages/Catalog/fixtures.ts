92402a7fc8d9f9438b838de55de995477e1c96cc
Dirley Pinto <dirley.figueredo@gmail.com>
2022-05-27 19:46:44 -0300

    update should show toast and redirect when submit form correctly

--- frontweb/src/pages/Catalog/fixtures.ts
+++ frontweb/src/pages/Catalog/fixtures.ts
@@ -5,6 +5,24 @@ import { BASE_URL } from 'util/requests';
 const findAllResponse = {
     "content": [
         {
+            "id": 2,
+            "name": "Smart TV",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "price": 2190.0,
+            "imgUrl": "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
+            "date": "2020-07-14T10:00:00Z",
+            "categories": [
+                {
+                    "id": 3,
+                    "name": "Computadores"
+                },
+                {
+                    "id": 1,
+                    "name": "Livros"
+                }
+            ]
+        },
+        {
             "id": 20,
             "name": "PC Gamer Tr",
             "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
